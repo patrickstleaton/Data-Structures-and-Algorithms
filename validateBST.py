a353b4527f1bbe81cf13b74c59ae60816b7e3db7
Patrick Leaton <patrickstleaton@hotmail.com>
2020-05-23 08:28:10 -0700

    Add files via upload

---
+++ validateBST.py
@@ -0,0 +1,18 @@
+class BST:
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
+
+def validateBST(tree):
+	return validateBSTHelper(tree, float('-inf'), float('inf'))
+
+def validateBSTHelper(tree, minValue, maxValue):
+	if tree is None:
+		return True
+	if tree.value < minValue or tree.value >= maxValue:
+		return False
+	leftIsValid = validateBSTHelper(tree.left, minValue, tree.value)
+	rightIsValid = validateBSTHelper(tree.right, tree.value, maxValue)
+	return leftIsValid and rightIsValid
+ 

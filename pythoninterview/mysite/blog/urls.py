941ca6ccdeea6839e00ee42971386da87b472a16
Patrick <patrickstleaton@hotmail.com>
2020-09-24 17:20:20 -0700

    Added category and difficulty views, set badge links to the views, updated Querysets and fixed navbar padding

--- pythoninterview/mysite/blog/urls.py
+++ pythoninterview/mysite/blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
 path('', views.PostListView.as_view(), name='post_list'),
+path('category/<str:category>/', views.CategoryPostListView.as_view(), name='category_post_list'),
+path('difficulty/<str:difficulty>/', views.DifficultyPostListView.as_view(), name='difficulty_post_list'),
 path('admin/', views.AboutView.as_view(), name='about'),
 path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- pythoninterview/mysite/blog/views.py
+++ pythoninterview/mysite/blog/views.py
@@ -2,7 +2,7 @@
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
+from django.views.generic import (TemplateView, ListView, DetailView)
 from blog.forms import PostForm
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -16,7 +16,22 @@ class AboutView(TemplateView):
 class PostListView(ListView):
     model = Post
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+        return Post.objects.all().order_by('title')
+
+class CategoryPostListView(ListView):
+    model = Post
+    def get_queryset(self, *args, **kwargs):
+        category = self.kwargs.get('category')
+        main_category = Post.objects.filter(category = category)
+        alt_category = Post.objects.filter(alt_category = category)
+        categories = main_category | alt_category
+        return categories
+
+class DifficultyPostListView(ListView):
+    model = Post
+    def get_queryset(self, *args, **kwargs):
+        difficulty = self.kwargs.get('difficulty')
+        return Post.objects.filter(difficulty = difficulty)
 
 class PostDetailView(DetailView):
     model = Post

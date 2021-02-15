70758eb2fe5acbb6483022af2df3e0b11d8a746e
Patrick <patrickstleaton@hotmail.com>
2021-02-15 15:18:08 -0700

    Added domains

--- pythoninterview/mysite/mysite/settings.py
+++ pythoninterview/mysite/mysite/settings.py
@@ -22,7 +22,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'blog/templates/blog')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['67.205.165.239']
+ALLOWED_HOSTS = ['67.205.165.239', 'www.algoninjutsu.com', 'algoninjutsu.com']
 
 # Application definition
 INSTALLED_APPS = [

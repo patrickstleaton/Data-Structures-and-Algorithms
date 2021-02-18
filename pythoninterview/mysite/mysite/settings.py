c72c0e38e2d08b8ff19142030950e8d6d4c23555
Patrick <patrickstleaton@hotmail.com>
2021-02-18 00:08:59 -0700

    made viewport fluid

--- pythoninterview/mysite/mysite/settings.py
+++ pythoninterview/mysite/mysite/settings.py
@@ -15,8 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'blog/templates/blog')
-# SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = True
+
+DEBUG = False
 
 ALLOWED_HOSTS = ['67.205.165.239', 'www.algoninjutsu.com', 'algoninjutsu.com', '127.0.0.1']
 

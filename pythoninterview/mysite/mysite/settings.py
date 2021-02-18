79d44ececb62aca0107446a0c7eb516f42339fc2
Patrick <patrickstleaton@hotmail.com>
2021-02-17 17:58:09 -0700

    fixed viewport styling

--- pythoninterview/mysite/mysite/settings.py
+++ pythoninterview/mysite/mysite/settings.py
@@ -18,7 +18,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'blog/templates/blog')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
+# SECURITY WARNING: keep the secret key used in production secret!
 DEBUG = False
 
 ALLOWED_HOSTS = ['67.205.165.239', 'www.algoninjutsu.com', 'algoninjutsu.com', '127.0.0.1']
